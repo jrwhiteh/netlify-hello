33a41e29c409606b8fc20b37ca6e2617a9880fd1
j <j>
2022-12-21 09:43:13 -0500

    env setup

--- functions/readAll.js
+++ functions/readAll.js
@@ -1,13 +1,13 @@
 // Grab our credentials from a .env file or environment variables
 require('dotenv').config();
 const {
-    VITE_PUBLIC_DATABASE_URL,
-    VITE_PUBLIC_SUPABASE_SERVICE_API_KEY
+    DATABASE_URL,
+    SUPABASE_SERVICE_API_KEY
 } = process.env;
 
 // Connect to our database 
 const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient(VITE_PUBLIC_DATABASE_URL, VITE_PUBLIC_SUPABASE_SERVICE_API_KEY);
+const supabase = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY);
 
 // Our standard serverless handler function
 exports.handler = async event => {
